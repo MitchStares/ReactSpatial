1460dfac202be38c58f8a68691ef25363d07de4f
Mitch Stares <mitchell.stares@hotmail.com>
2023-05-26 13:53:24 +1000

    Tweaking sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import "./Sidebar.css";
-import AppBar from '@mui/material/AppBar';
+import {AppBar, Tabs, Tab, Box} from '@mui/material/';
 
-const Sidebar = ({ isOpen, toggleSidebar }) => {
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+const Sidebar = ({ isOpen, toggleSidebar}) => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
    return (
     <div className="sidebar" style={{ width: isOpen ? '35vw' : '0' }}>
       <button className="sidebar-toggle" onClick={toggleSidebar}>
@@ -10,16 +26,16 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
       </button>
       {isOpen && (
         <div className="sidebar-content">
-          <AppBar position='absolute'>
+          <AppBar position='static'>
             <header className="sidebar-header"><p>FocalPoint</p></header>
           </AppBar>
-          
-          <div className='sidebar-topMenu'>
-            <div>
-              <a href="/">Information</a>
-              <a href='/'>Insights</a>
-            </div>
-          </div>
+          {/* <Box sx={{ borderBottom: 1, borderColor: 'divider', display:'flex', position:'relative'}}> */}
+            <Tabs value={value} onChange={handleChange} variant="fullWidth" centered>
+              <LinkTab label='Information' href='info'/>
+              <LinkTab label='Data' href='/data'/>
+            </Tabs>
+          {/* </Box> */}
+                    
           {"Text"/* Sidebar content goes here */}
         </div>
       )}
